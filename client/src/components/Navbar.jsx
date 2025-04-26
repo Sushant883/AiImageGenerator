8fbbb5e24838aac1ebd5fe98b6ca06cccb578226
Sushant <panditsushant11@gmail.com>
2025-04-26 12:42:18 +0530

    add navbar or button file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import { AddRounded } from "@mui/icons-material";
 
 const Container = styled.div`
-color: white;
+  flex: 1;
+  background: ${({ theme }) => theme.navbar};
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: bold;
+  font-size: 22px;
+  padding: 14px 50px;
+  display: flex;
+  justify-content: space-between;
+  align-item: center;
+  box-shadow: 0,0,10px,rgba (0,0,0,0.15);
 `;
 
 function Navbar() {
   return (
-    <Container>Navbar</Container>
-  )
+    <Container>
+      GenAI
+      <Button
+        text="Create new post"
+        leftIcon={
+          <AddRounded
+            style={{
+              fontSize: "18px",
+            }}
+          />
+        }
+      />
+    </Container>
+  );
 }
 
-export default Navbar
+export default Navbar;
