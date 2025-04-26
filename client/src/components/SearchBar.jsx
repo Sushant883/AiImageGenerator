bf36cfd7cefa8ad6e48c2b1e29411bc4206d9e69
Sushant <panditsushant11@gmail.com>
2025-04-26 13:49:25 +0530

    add searchbar file

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { SearchOutlined } from "@mui/icons-material";
+import styled from "styled-components";
+
+const SearchBarContainer = styled.div`
+max-width:550px;
+display: flex;
+width: 90%;
+border: 1px solid ${({theme}) => theme.text_secondary + 90};
+color: ${({theme}) => theme.text_primary};
+border-radius:8px;
+padding:12px 16px;
+cursor: pointer;
+gap: 6px;
+align-item: center;
+`;
 
 const SearchBar = () => {
   return (
-    <div>
-      Searchbar
-    </div>
-  )
-}
+    <SearchBarContainer>
+      <SearchOutlined />
+      <input 
+      style={{
+        border: "none",
+        outline:"none",
+        width :"100%",
+        color: "inherit",
+        background: "transparent",
+        // fontSize: "18px"
+      }}
+      />
+    </SearchBarContainer>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
