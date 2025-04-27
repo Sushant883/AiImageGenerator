1b7d542cd1ffa6f41d902c425b1fe47c705fd5c5
Sushant <panditsushant11@gmail.com>
2025-04-27 14:36:36 +0530

    Create a Form section in CreatePost file

---
+++ client/src/components/GenerateImageForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const GenerateImageForm = () => {
+  return (
+    <Form>
+      <Top>
+        <Title>Generate Image with prompt</Title>
+        <Desc>
+          Write your prompt according to the image you want to Generate !
+        </Desc>
+      </Top>
+    </Form>
+  );
+};
+
+export default GenerateImageForm;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import GenerateImageForm from "../components/GenerateImageForm";
 
 const Container = styled.div`
   height: 100%;
+  width: 100%;
   overflow-y: scroll;
+  background : ${({ theme }) => theme.bg}
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display:flex;
+  flex-direction: column;
+  // border: 2px solid blue;
+  align-items: center;
+  gap: 20px;
+  @media(max-width: 768px) {
+  padding: 6px 10px;
+  }
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  height: fit-content;
+  max-width: 1200px;
+  gap: 8%;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 function CreatePost() {
-  return <Container>Create Post</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        
+         <GenerateImageForm /> {/* this is Generateimage form components */}
+        
+        </Wrapper>
+    </Container>
+  );
 }
 
 export default CreatePost;
