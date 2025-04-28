01462438e74c236c7e87aaba0ff9f3c3710dd91d
Sushant <panditsushant11@gmail.com>
2025-04-28 19:48:03 +0530

    add home page API

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 import cors from "cors";
+import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dontenv";
 
@@ -7,3 +8,19 @@ dotenv.config;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something Went wrong!";
+  return res.status(status).json({ success: false, status, message });
+});
+
+
+//Default get
+app.get("/", async (req,res) =>{
+    res.status(200).json({
+        message: "Hello GFG Develovers!",
+    })
+})
