bbf0a24eab0976105190c93a95325b9911f58b85
Sushant <panditsushant11@gmail.com>
2025-04-29 13:50:53 +0530

    add route file

--- Server/controllers/GenerateAIImage.js
+++ Server/controllers/GenerateAIImage.js
@@ -12,28 +12,28 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-
 // controller to generate Image
 
-export const generateImage = async(req,res,next) =>{
-    try{
-        const {prompt} = req.body;
+export const generateImage = async (req, res, next) => {
+  try {
+    const { prompt } = req.body;
 
-        const response = await openai.createImage({
-            prompt,
-            n:1,
-            size: "1024x1024",
-            response_format: "b64json",
-        });
-        const generateImage = response.data.data[0].b64_json;
-    }catch(error){
-        next(
-            createError(
-              error.status,
-              error?.response?.data?.error?.message ||
-                error?.message ||
-                error?.message
-            )
-        )
-    }
-}
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64json",
+    });
+    const generateImage = response.data.data[0].b64_json;
+    return res.status(200).json({ photo: generateImage });
+  } catch (error) {
+    next(
+      createError(
+        error.status,
+        error?.response?.data?.error?.message ||
+          error?.message ||
+          error?.message
+      )
+    );
+  }
+};
