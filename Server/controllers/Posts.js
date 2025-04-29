c04bc6935393215e9d00f1cd2493588727d8a322
Sushant <panditsushant11@gmail.com>
2025-04-29 12:22:21 +0530

    add add backend code

--- Server/controllers/Posts.js
+++ Server/controllers/Posts.js
@@ -5,11 +5,11 @@ import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
-// cloudinary.config({
-//     cloud_name: "",
-//     api_key: "",
-//     api_secret: "",
-// });
+cloudinary.config({
+    cloud_name: "",
+    api_key: "",
+    api_secret: "",
+});
 
 // Get all posts
 
@@ -33,16 +33,16 @@ export const getAllPosts = async (req, res, next) => {
 
 export const createPost = async (req, res) => {
   try {
-const {name, prompt, photo} = req.body;
-const photoUrl = "";
-const newPost = await Post.create({
-    name,
-    prompt,
-    photo: photoUrl,
-});
-
+    const { name, prompt, photo } = req.body;
+    const photoUrl = "";
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl,
+    });
+    return res.status(201).json({ success: true, data: newPost });
   } catch (error) {
-    next(
+    next( 
       createError(
         error.status,
         error?.response?.data?.error?.message ||
