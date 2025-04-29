a19942a7adb027224be1e18d732f47da1bbd0f2a
Sushant <panditsushant11@gmail.com>
2025-04-29 13:47:09 +0530

    add api code to the file

--- Server/controllers/GenerateAIImage.js
+++ Server/controllers/GenerateAIImage.js
@@ -1,6 +1,6 @@
 import * as dontenv from "dotenv";
 import { createError } from "../error.js";
-import { Configuration } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
 dontenv.config();
 
@@ -9,3 +9,31 @@ dontenv.config();
 const configuration = new Configuration({
   apiKey: process.env.OPEN_AI_KEY,
 });
+
+const openai = new OpenAIApi(configuration);
+
+
+// controller to generate Image
+
+export const generateImage = async(req,res,next) =>{
+    try{
+        const {prompt} = req.body;
+
+        const response = await openai.createImage({
+            prompt,
+            n:1,
+            size: "1024x1024",
+            response_format: "b64json",
+        });
+        const generateImage = response.data.data[0].b64_json;
+    }catch(error){
+        next(
+            createError(
+              error.status,
+              error?.response?.data?.error?.message ||
+                error?.message ||
+                error?.message
+            )
+        )
+    }
+}
