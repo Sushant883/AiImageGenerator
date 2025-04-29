2d5b53e66f80990c10f4acdd0a115c72b59ce95c
Sushant <panditsushant11@gmail.com>
2025-04-29 12:16:39 +0530

    add reute files  model files or photo url

--- Server/controllers/Posts.js
+++ Server/controllers/Posts.js
@@ -11,22 +11,44 @@ dotenv.config();
 //     api_secret: "",
 // });
 
-
-
 // Get all posts
 
-export const getAllPosts = async(req, res, next ) => {
-    try{
-        const posts = await Post.find({});
-        return res.status(200).json({success:true, data:posts}); 
-    }
-    catch(error) {
+export const getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    return res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    next(
+      createError(
+        error.status,
+        error?.response?.data?.error?.message ||
+          error?.message ||
+          error?.message
+      )
+    );
+  }
+};
 
-        next(
-            createError(
-                error.status,
-                error?.response?.data?.error?.message || error?.message || error?.message
-            )
-        )
-    }
-}
+// Create Post
+
+export const createPost = async (req, res) => {
+  try {
+const {name, prompt, photo} = req.body;
+const photoUrl = "";
+const newPost = await Post.create({
+    name,
+    prompt,
+    photo: photoUrl,
+});
+
+  } catch (error) {
+    next(
+      createError(
+        error.status,
+        error?.response?.data?.error?.message ||
+          error?.message ||
+          error?.message
+      )
+    );
+  }
+};
